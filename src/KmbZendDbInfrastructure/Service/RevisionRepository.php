966e259a44194e562c92cc0e97b7a580ee4a27a8
Emmanuel Bouton <goten4@gmail.com>
2014-10-29 01:29:00 +0100

    RevisionRepository::add() with a transaction

--- src/KmbZendDbInfrastructure/Service/RevisionRepository.php
+++ src/KmbZendDbInfrastructure/Service/RevisionRepository.php
@@ -29,6 +29,7 @@ use KmbDomain\Model\Revision;
 use KmbDomain\Model\RevisionInterface;
 use KmbDomain\Model\RevisionRepositoryInterface;
 use KmbZendDbInfrastructure\Proxy\EnvironmentProxy;
+use Zend\Db\Exception\ExceptionInterface;
 use Zend\Db\Adapter\Driver\ResultInterface;
 use Zend\Db\Sql\Select;
 use Zend\Db\Sql\Where;
@@ -54,17 +55,26 @@ class RevisionRepository extends Repository implements RevisionRepositoryInterfa
     /**
      * @param AggregateRootInterface $aggregateRoot
      * @return RepositoryInterface
+     * @throws \Zend\Db\Exception\ExceptionInterface
      */
     public function add(AggregateRootInterface $aggregateRoot)
     {
-        /** @var RevisionInterface $aggregateRoot */
-        parent::add($aggregateRoot);
+        /** @var EnvironmentInterface $aggregateRoot */
+        $connection = $this->getDbAdapter()->getDriver()->getConnection()->beginTransaction();
+        try {
+            /** @var RevisionInterface $aggregateRoot */
+            parent::add($aggregateRoot);
 
-        if ($aggregateRoot->hasGroups()) {
-            foreach ($aggregateRoot->getGroups() as $group) {
-                $group->setRevision($aggregateRoot);
-                $this->groupRepository->add($group);
+            if ($aggregateRoot->hasGroups()) {
+                foreach ($aggregateRoot->getGroups() as $group) {
+                    $group->setRevision($aggregateRoot);
+                    $this->groupRepository->add($group);
+                }
             }
+            $connection->commit();
+        } catch (ExceptionInterface $e) {
+            $connection->rollback();
+            throw $e;
         }
 
         return $this;
